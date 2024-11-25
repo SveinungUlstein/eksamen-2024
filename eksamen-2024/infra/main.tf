74f289394f2844f6ae0d478ddf12a344b21b7653
EC2 Default User <ec2-user@ip-172-31-35-239.eu-west-1.compute.internal>
2024-11-25 15:13:10 +0000

    made new tf file for cw2

--- eksamen-2024/infra/main.tf
+++ eksamen-2024/infra/main.tf
@@ -128,36 +128,3 @@ output "lambda_url" {
 output "sqs_queue_url" {
   value = aws_sqs_queue.lambda_queue.id
 }
-
-variable "notification_email" {
-  description = "seinung.ulstein@gmail.com"
-  type        = string
-}
-
-resource "aws_cloudwatch_metric_alarm" "sqs_oldest_message_age" {
-  alarm_name                = "sqs-alarm-oldest-message-27"
-  comparison_operator       = "GreaterThanThreshold"
-  evaluation_periods        = 1
-  metric_name               = "ApproximateAgeOfOldestMessage"
-  namespace                 = "AWS/SQS"
-  period                    = 20
-  statistic                 = "Maximum"
-  threshold                 = 1 
-  alarm_description         = "Alarm når den eldste meldingen i SQS er eldre enn 5 minutter"
-  dimensions = {
-    QueueName = "lambda_queue_27"
-  }
-
-  # Send varsler via SNS
-  alarm_actions = [aws_sns_topic.sqs_alarm_topic.arn]
-}
-
-resource "aws_sns_topic" "sqs_alarm_topic" {
-  name = "sqs-alarm-topic"
-}
-
-resource "aws_sns_topic_subscription" "email_subscription" {
-  topic_arn = aws_sns_topic.sqs_alarm_topic.arn
-  protocol  = "email"
-  endpoint  = var.notification_email
-}
